c8ae64ae730e61ddeac569edf21e3b4a81c08e35
ritunjay kumar <ritunjaykumar7777@gmail.com>
2021-07-09 09:38:30 +0530

    standalone collection

---
+++ src/main/java/com/softgyan/standalon/collection/Main.java
@@ -0,0 +1,15 @@
+package com.softgyan.standalon.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        final String CONFIG_PATH = "classpath:standalon_collection.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(CONFIG_PATH);
+        Person person = applicationContext.getBean("person", Person.class);
+        System.out.println(person);
+        System.out.println(".................................");
+        System.out.println(person.getFriends().getClass().getName());
+    }
+}
---
+++ src/main/java/com/softgyan/standalon/collection/Person.java
@@ -0,0 +1,47 @@
+package com.softgyan.standalon.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class Person {
+    private List<String> friends;
+    private Map<String, Integer> feeStructures;
+    private Properties properties;
+    public Person() {
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "friends=" + friends +
+                "\nfeeStructures=" + feeStructures +
+                "\nproperties=" + properties +
+                '}';
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Map<String, Integer> getFeeStructures() {
+        return feeStructures;
+    }
+
+    public void setFeeStructures(Map<String, Integer> feeStructures) {
+        this.feeStructures = feeStructures;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+}
